ea048900365729d9c3833c65bd2e4a5a491cc849
Denis Nikiforov <popalay@ex.ua>
2017-03-04 00:47:09 +0200

    Update icons Add lift ot touch animation

--- app/src/main/java/com/popalay/yocard/ui/base/widgets/CharacterWrapTextView.java
+++ app/src/main/java/com/popalay/yocard/ui/base/widgets/CharacterWrapTextView.java
@@ -1,77 +1,41 @@
 package com.popalay.yocard.ui.base.widgets;
 
 import android.content.Context;
-import android.text.Spannable;
-import android.text.SpannableString;
-import android.text.style.ScaleXSpan;
+import android.graphics.Rect;
 import android.util.AttributeSet;
 
 public class CharacterWrapTextView extends android.support.v7.widget.AppCompatTextView {
-
-    private float letterSpacing = LetterSpacing.BIGGEST;
-    private CharSequence originalText = "";
-
 
     public CharacterWrapTextView(Context context) {
         super(context);
     }
 
-    public CharacterWrapTextView(Context context, AttributeSet attrs){
+    public CharacterWrapTextView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        originalText = super.getText();
-        applyLetterSpacing();
-        this.invalidate();
     }
 
-    public CharacterWrapTextView(Context context, AttributeSet attrs, int defStyle){
+    public CharacterWrapTextView(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
-    }
-
-    public float getLetterSpacing() {
-        return letterSpacing;
-    }
-
-    public void setLetterSpacing(float letterSpacing) {
-        this.letterSpacing = letterSpacing;
-        applyLetterSpacing();
     }
 
     @Override
     public void setText(CharSequence text, BufferType type) {
-        originalText = text;
+        super.setText(text, type);
         applyLetterSpacing();
     }
 
-    @Override
-    public CharSequence getText() {
-        return originalText;
-    }
-
     private void applyLetterSpacing() {
-        if (this == null || this.originalText == null) return;
-        StringBuilder builder = new StringBuilder();
-        for(int i = 0; i < originalText.length(); i++) {
-            String c = ""+ originalText.charAt(i);
-            builder.append(c.toLowerCase());
-            if(i+1 < originalText.length()) {
-                builder.append("\u00A0");
+        final Rect bounds = new Rect();
+        getPaint().getTextBounds(getText().toString(), 0, getText().length(), bounds);
+        final int textWidth = bounds.width();
+        post(() -> {
+            final int width = getWidth();
+            if (width == 0) {
+                return;
             }
-        }
-        SpannableString finalText = new SpannableString(builder.toString());
-        if(builder.toString().length() > 1) {
-            for(int i = 1; i < builder.toString().length(); i+=2) {
-                finalText.setSpan(new ScaleXSpan((letterSpacing+1)/10), i, i+1, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-            }
-        }
-        super.setText(finalText, BufferType.SPANNABLE);
-    }
+            setLetterSpacing((float) textWidth / width);
+        });
 
-    public class LetterSpacing {
-        public final static float NORMAL = 0;
-        public final static float NORMALBIG = (float)0.025;
-        public final static float BIG = (float)0.05;
-        public final static float BIGGEST = (float)0.2;
     }
-
 
 }
