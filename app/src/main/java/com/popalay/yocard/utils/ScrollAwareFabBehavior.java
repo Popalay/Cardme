746060b8225b927c7188fff44c3813fc2349b004
Denis Nikiforov <popalay@ex.ua>
2017-03-07 00:59:44 +0200

    Fix fab scroll behavior

--- app/src/main/java/com/popalay/yocard/utils/ScrollAwareFabBehavior.java
+++ app/src/main/java/com/popalay/yocard/utils/ScrollAwareFabBehavior.java
@@ -3,9 +3,10 @@ package com.popalay.yocard.utils;
 import android.content.Context;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v13.view.ViewCompat;
 import android.util.AttributeSet;
 import android.view.View;
+import android.view.animation.AccelerateDecelerateInterpolator;
 
 public class ScrollAwareFabBehavior extends SnackbarFabBehavior {
 
@@ -14,14 +15,38 @@ public class ScrollAwareFabBehavior extends SnackbarFabBehavior {
     }
 
     @Override
-    public boolean layoutDependsOn(CoordinatorLayout parent, FloatingActionButton child, View dependency) {
-        return dependency instanceof Snackbar.SnackbarLayout;
+    public void onNestedScroll(CoordinatorLayout coordinatorLayout,
+            FloatingActionButton child,
+            View target,
+            int dxConsumed,
+            int dyConsumed,
+            int dxUnconsumed,
+            int dyUnconsumed) {
+        super.onNestedScroll(coordinatorLayout, child, target, dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed);
+
+        if (dyConsumed > 0) {
+            CoordinatorLayout.LayoutParams layoutParams = (CoordinatorLayout.LayoutParams) child.getLayoutParams();
+            int fab_bottomMargin = layoutParams.bottomMargin;
+            child.animate()
+                    .translationY(child.getHeight() + fab_bottomMargin)
+                    .setInterpolator(new AccelerateDecelerateInterpolator())
+                    .setDuration(300L)
+                    .start();
+        } else if (dyConsumed < 0) {
+            child.animate()
+                    .translationY(0)
+                    .setInterpolator(new AccelerateDecelerateInterpolator())
+                    .setDuration(300L)
+                    .start();
+        }
     }
 
     @Override
-    public boolean onDependentViewChanged(CoordinatorLayout parent, FloatingActionButton child, View dependency) {
-        float translationY = Math.min(0, dependency.getTranslationY() - dependency.getHeight());
-        child.setTranslationY(translationY);
-        return true;
+    public boolean onStartNestedScroll(CoordinatorLayout coordinatorLayout,
+            FloatingActionButton child,
+            View directTargetChild,
+            View target,
+            int nestedScrollAxes) {
+        return nestedScrollAxes == ViewCompat.SCROLL_AXIS_VERTICAL;
     }
 }
