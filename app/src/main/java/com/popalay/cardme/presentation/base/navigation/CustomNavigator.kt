cc6a8248777cf70e32f9d6cf768597b9d1d122fa
Denis Nikiforov <popalay@ex.ua>
2017-07-11 11:53:08 +0300

    Add animation to fragment transactions

--- app/src/main/java/com/popalay/cardme/presentation/base/navigation/CustomNavigator.kt
+++ app/src/main/java/com/popalay/cardme/presentation/base/navigation/CustomNavigator.kt
@@ -2,16 +2,23 @@ package com.popalay.cardme.presentation.base.navigation
 
 import android.content.Intent
 import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
 import android.widget.Toast
+import com.popalay.cardme.R
 import com.popalay.cardme.business.exception.AppException
 import com.popalay.cardme.presentation.base.BaseActivity
 import ru.terrakok.cicerone.android.SupportAppNavigator
+import ru.terrakok.cicerone.commands.Back
 import ru.terrakok.cicerone.commands.Command
+import ru.terrakok.cicerone.commands.Forward
+import ru.terrakok.cicerone.commands.Replace
 
 abstract class CustomNavigator(
         val activity: BaseActivity,
-        container: Int = 0
-) : SupportAppNavigator(activity, container) {
+        val containerId: Int = 0
+) : SupportAppNavigator(activity, containerId) {
+
+    val fragmentManager: FragmentManager = activity.supportFragmentManager
 
     override fun applyCommand(command: Command?) {
         if (command is Error) {
@@ -28,16 +35,81 @@ abstract class CustomNavigator(
             }
         }
 
+        if (command is Forward) {
+            val forward = command
+            val activityIntent = createActivityIntent(forward.screenKey, forward.transitionData)
+
+            // Start activity
+            if (activityIntent != null) {
+                activity.startActivity(activityIntent)
+                return
+            }
+        } else if (command is Replace) {
+            val replace = command
+            val activityIntent = createActivityIntent(replace.screenKey, replace.transitionData)
+
+            // Replace activity
+            if (activityIntent != null) {
+                activity.startActivity(activityIntent)
+                activity.finish()
+                return
+            }
+        }
+
+        if (command is Forward) {
+            val forward = command
+            val fragment = createFragment(forward.screenKey, forward.transitionData)
+            if (fragment == null) {
+                unknownScreen(command)
+                return
+            }
+            fragmentManager
+                    .beginTransaction()
+                    .setCustomAnimations(R.anim.fade_in, R.anim.fade_out)
+                    .replace(containerId, fragment)
+                    .addToBackStack(forward.screenKey)
+                    .commit()
+            return
+        } else if (command is Back) {
+            if (fragmentManager.backStackEntryCount > 0) {
+                fragmentManager.popBackStackImmediate()
+                return
+            } else {
+                exit()
+                return
+            }
+        } else if (command is Replace) {
+            val replace = command
+            val fragment = createFragment(replace.screenKey, replace.transitionData)
+            if (fragment == null) {
+                unknownScreen(command)
+                return
+            }
+            if (fragmentManager.backStackEntryCount > 0) {
+                fragmentManager.popBackStackImmediate()
+                fragmentManager
+                        .beginTransaction()
+                        .setCustomAnimations(R.anim.fade_in, R.anim.fade_out)
+                        .replace(containerId, fragment)
+                        .addToBackStack(replace.screenKey)
+                        .commit()
+                return
+            } else {
+                fragmentManager
+                        .beginTransaction()
+                        .setCustomAnimations(R.anim.fade_in, R.anim.fade_out)
+                        .replace(containerId, fragment)
+                        .commit()
+                return
+            }
+        }
+
         super.applyCommand(command)
     }
 
-    override fun createFragment(screenKey: String, data: Any?): Fragment? {
-        return null
-    }
+    override fun createFragment(screenKey: String, data: Any?): Fragment? = null
 
-    override fun createActivityIntent(screenKey: String, data: Any?): Intent? {
-        return null
-    }
+    override fun createActivityIntent(screenKey: String, data: Any?): Intent? = null
 
     open fun showError(exception: AppException) {
         Toast.makeText(activity, exception.messageRes, Toast.LENGTH_LONG).show()
