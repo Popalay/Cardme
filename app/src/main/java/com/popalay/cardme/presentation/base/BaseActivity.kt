c64d5c1f3f04943a801e755bf578a85722d6a975
Denis Nikiforov <popalay@ex.ua>
2017-07-14 17:45:07 +0300

    Remove Moxy from project

--- app/src/main/java/com/popalay/cardme/presentation/base/BaseActivity.kt
+++ app/src/main/java/com/popalay/cardme/presentation/base/BaseActivity.kt
@@ -1,40 +1,18 @@
 package com.popalay.cardme.presentation.base
 
-import android.app.ProgressDialog
-import android.support.annotation.StringRes
-import android.widget.Toast
-import com.arellomobile.mvp.MvpAppCompatActivity
-import com.popalay.cardme.R
+import android.support.v7.app.AppCompatActivity
 import com.popalay.cardme.injection.Injectable
 import com.popalay.cardme.presentation.base.navigation.CustomNavigator
-import com.popalay.cardme.utils.DialogFactory
-import com.popalay.cardme.utils.ViewUtil
 import io.reactivex.disposables.CompositeDisposable
-import io.reactivex.disposables.Disposable
 import ru.terrakok.cicerone.NavigatorHolder
 import javax.inject.Inject
 
-abstract class BaseActivity : MvpAppCompatActivity(), BaseView, Injectable{
+abstract class BaseActivity : AppCompatActivity(), Injectable {
 
     @Inject lateinit var navigationHolder: NavigatorHolder
 
     protected val disposables: CompositeDisposable by lazy { CompositeDisposable() }
     protected open val navigator = CustomNavigator(this)
-
-    private val progressDialog: ProgressDialog by lazy {
-        DialogFactory.createProgressDialog(this, R.string.please_wait)
-    }
-
-    private val subscriptions = CompositeDisposable()
-
-    fun showLoadingDialog() = progressDialog.show()
-
-    fun addDisposable(disposable: Disposable) {
-        if (disposable.isDisposed) return
-        subscriptions.add(disposable)
-    }
-
-    fun hideLoadingDialog() = progressDialog.dismiss()
 
     override fun onResumeFragments() {
         super.onResumeFragments()
@@ -47,25 +25,7 @@ abstract class BaseActivity : MvpAppCompatActivity(), BaseView, Injectable{
     }
 
     override fun onDestroy() {
-        subscriptions.clear()
+        disposables.clear()
         super.onDestroy()
     }
-
-    override fun showError(error: String) = DialogFactory.createSimpleOkErrorDialog(this, error).show()
-
-    override fun showError(@StringRes error: Int) = showError(getString(error))
-
-    override fun showMessage(message: String) = Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
-
-    override fun hideError() {}
-
-    override fun hideMessage() {}
-
-    override fun showProgress() = showLoadingDialog()
-
-    override fun hideProgress() = hideLoadingDialog()
-
-    override fun hideKeyboard() = ViewUtil.hideKeyboard(this)
-
-    override fun close() = finish()
 }
--- app/src/main/java/com/popalay/cardme/presentation/base/BaseFragment.kt
+++ app/src/main/java/com/popalay/cardme/presentation/base/BaseFragment.kt
@@ -1,47 +1,16 @@
 package com.popalay.cardme.presentation.base
 
-import android.support.annotation.StringRes
-import com.arellomobile.mvp.MvpAppCompatFragment
+import android.support.v4.app.Fragment
 import com.popalay.cardme.injection.Injectable
 import io.reactivex.disposables.CompositeDisposable
 
-abstract class BaseFragment : MvpAppCompatFragment(), BaseView, Injectable {
+abstract class BaseFragment : Fragment(), Injectable {
 
     protected val disposables: CompositeDisposable by lazy { CompositeDisposable() }
-
-    fun baseActivity(): BaseActivity {
-        val activity = activity
-        if (activity != null && activity is BaseActivity) {
-            return activity
-        }
-        throw RuntimeException("BaseActivity is null")
-    }
 
     override fun onDestroyView() {
         disposables.clear()
         super.onDestroyView()
     }
-
-    protected fun showLoadingDialog() = baseActivity().showLoadingDialog()
-
-    protected fun hideLoadingDialog() = baseActivity().hideLoadingDialog()
-
-    override fun showError(error: String) = baseActivity().showError(error)
-
-    override fun showError(@StringRes error: Int) = showError(getString(error))
-
-    override fun showMessage(message: String) = baseActivity().showMessage(message)
-
-    override fun hideError() {}
-
-    override fun hideMessage() {}
-
-    override fun showProgress() = showLoadingDialog()
-
-    override fun hideProgress() = hideLoadingDialog()
-
-    override fun hideKeyboard() = baseActivity().hideKeyboard()
-
-    override fun close(): Unit = throw UnsupportedOperationException("You can not close the fragment")
 
 }
