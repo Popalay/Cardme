ad5e4a00c1c5d03b0b1c621abb3b698d24dac63b
Denis Nikiforov <popalay@ex.ua>
2017-07-16 21:58:46 +0300

    Fix empty string bug

--- app/src/main/java/com/popalay/cardme/utils/StringUtils.kt
+++ app/src/main/java/com/popalay/cardme/utils/StringUtils.kt
@@ -1,5 +1,6 @@
 package com.popalay.cardme.utils
 
-fun String.firstLetters(): String = this.split(" ")
-        .map { it[0].toString() }
-        .reduce { acc, s -> acc + s }
+fun String.firstLetters(): String {
+    val splits = split(" ")
+    return if (splits.isEmpty()) "" else splits.map { it[0].toString() }.reduce { acc, s -> acc + s }
+}
