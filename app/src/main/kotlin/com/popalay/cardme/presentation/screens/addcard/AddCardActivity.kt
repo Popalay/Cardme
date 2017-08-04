8bfa17c9c5d29b4ab98f811ae113d85a72ceb3d1
Denis Nikiforov <popalay@ex.ua>
2017-08-04 15:23:50 +0300

    Replace button to menu action on the add card screen

--- app/src/main/kotlin/com/popalay/cardme/presentation/screens/addcard/AddCardActivity.kt
+++ app/src/main/kotlin/com/popalay/cardme/presentation/screens/addcard/AddCardActivity.kt
@@ -6,28 +6,53 @@ import android.content.Context
 import android.content.Intent
 import android.databinding.DataBindingUtil
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import com.popalay.cardme.R
 import com.popalay.cardme.databinding.ActivityAddCardBinding
 import com.popalay.cardme.presentation.base.RightSlidingActivity
+import io.reactivex.rxkotlin.addTo
 import javax.inject.Inject
 
 class AddCardActivity : RightSlidingActivity() {
 
+    companion object {
+        fun getIntent(context: Context) = Intent(context, AddCardActivity::class.java)
+    }
+
     @Inject lateinit var factory: ViewModelProvider.Factory
 
     private lateinit var b: ActivityAddCardBinding
-
-    companion object {
-
-        fun getIntent(context: Context) = Intent(context, AddCardActivity::class.java)
-    }
+    private lateinit var viewModelFacade: AddCardViewModelFacade
+    private lateinit var acceptMenuItem: MenuItem
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         b = DataBindingUtil.setContentView<ActivityAddCardBinding>(this, R.layout.activity_add_card)
-        b.vm = ViewModelProviders.of(this, factory).get(AddCardViewModel::class.java)
+        ViewModelProviders.of(this, factory).get(AddCardViewModel::class.java).let {
+            b.vm = it
+            viewModelFacade = it
+        }
         initUI()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.accept_menu, menu)
+        acceptMenuItem = menu.findItem(R.id.action_accept)
+
+        viewModelFacade.onCanSaveStateChanged()
+                .subscribe { acceptMenuItem.isEnabled = it }
+                .addTo(disposables)
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_accept -> viewModelFacade.onAcceptClick()
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     override fun getRootView(): View = b.root
--- app/src/main/kotlin/com/popalay/cardme/presentation/screens/addcard/AddCardViewModel.kt
+++ app/src/main/kotlin/com/popalay/cardme/presentation/screens/addcard/AddCardViewModel.kt
@@ -3,8 +3,8 @@ package com.popalay.cardme.presentation.screens.addcard
 import android.databinding.ObservableArrayList
 import android.databinding.ObservableBoolean
 import android.databinding.ObservableField
-import android.databinding.ObservableList
 import android.view.inputmethod.EditorInfo
+import com.jakewharton.rxrelay2.BehaviorRelay
 import com.jakewharton.rxrelay2.PublishRelay
 import com.popalay.cardme.business.cards.CardInteractor
 import com.popalay.cardme.business.holders.HolderInteractor
@@ -18,7 +18,9 @@ import com.popalay.cardme.utils.extensions.setTo
 import com.stepango.rxdatabindings.ObservableString
 import com.stepango.rxdatabindings.observe
 import com.stepango.rxdatabindings.setTo
+import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.rxkotlin.Observables
 import io.reactivex.rxkotlin.addTo
 import io.reactivex.rxkotlin.subscribeBy
 import javax.inject.Inject
@@ -28,15 +30,15 @@ class AddCardViewModel @Inject constructor(
         cardInteractor: CardInteractor,
         holderInteractor: HolderInteractor,
         settingsInteractor: SettingsInteractor
-) : BaseViewModel() {
+) : BaseViewModel(), AddCardViewModelFacade {
 
     val holderName = ObservableString()
-    val holderNames: ObservableList<String> = ObservableArrayList<String>()
+    val holderNames: ObservableArrayList<String> = ObservableArrayList()
     val title = ObservableString()
-    val canSave = ObservableBoolean()
     val showImage = ObservableBoolean()
     val card = ObservableField<Card>()
 
+    val canSaveState: BehaviorRelay<Boolean> = BehaviorRelay.create<Boolean>()
     val acceptClickListener: PublishRelay<Boolean> = PublishRelay.create<Boolean>()
     val editorActionListener: PublishRelay<Int> = PublishRelay.create<Int>()
 
@@ -59,14 +61,11 @@ class AddCardViewModel @Inject constructor(
                 .subscribeBy(this::handleBaseError)
                 .addTo(disposables)
 
-        holderName.observe()
-                .switchMapSingle { cardInteractor.hasAllData(card.get(), it) }
-                .setTo(canSave)
-                .subscribeBy(this::handleBaseError)
-                .addTo(disposables)
-
-        title.observe()
-                .doOnNext { card.get()?.title = it.clean() }
+        Observables.combineLatest(holderName.observe(),
+                title.observe().doOnNext { card.get()?.title = it.clean() })
+                .switchMapSingle { cardInteractor.hasAllData(card.get(), it.first) }
+                .observeOn(AndroidSchedulers.mainThread())
+                .doOnNext(canSaveState::accept)
                 .subscribeBy(this::handleBaseError)
                 .addTo(disposables)
 
@@ -88,4 +87,13 @@ class AddCardViewModel @Inject constructor(
                 .addTo(disposables)
     }
 
+    override fun onAcceptClick() = acceptClickListener.accept(true)
+
+    override fun onCanSaveStateChanged(): Observable<Boolean> = canSaveState
+
+}
+
+interface AddCardViewModelFacade {
+    fun onAcceptClick()
+    fun onCanSaveStateChanged(): Observable<Boolean>
 }
