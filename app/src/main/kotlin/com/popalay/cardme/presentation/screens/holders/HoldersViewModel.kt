8bbbbb66869543c93de05e02191f9fb452d40051
Denis Nikiforov <popalay@ex.ua>
2017-08-06 21:59:31 +0300

    Avoid multiple click on a holder

--- app/src/main/kotlin/com/popalay/cardme/presentation/screens/holders/HoldersViewModel.kt
+++ app/src/main/kotlin/com/popalay/cardme/presentation/screens/holders/HoldersViewModel.kt
@@ -6,6 +6,7 @@ import com.popalay.cardme.data.models.Holder
 import com.popalay.cardme.presentation.base.BaseViewModel
 import com.popalay.cardme.presentation.base.navigation.CustomRouter
 import com.popalay.cardme.presentation.screens.SCREEN_HOLDER_DETAILS
+import com.popalay.cardme.utils.extensions.applyThrottling
 import com.popalay.cardme.utils.extensions.setTo
 import com.popalay.cardme.utils.recycler.DiffObservableList
 import io.reactivex.android.schedulers.AndroidSchedulers
@@ -29,6 +30,7 @@ class HoldersViewModel @Inject constructor(
                 .addTo(disposables)
 
         holderClickListener
+                .applyThrottling()
                 .doOnNext { router.navigateTo(SCREEN_HOLDER_DETAILS, it.name) }
                 .subscribeBy(this::handleBaseError)
                 .addTo(disposables)
