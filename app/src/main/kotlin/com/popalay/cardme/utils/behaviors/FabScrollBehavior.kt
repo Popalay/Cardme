54e1a79493b14e0220f93488fe7bb94f111013cc
Denis Nikiforov <popalay@ex.ua>
2017-08-14 13:33:58 +0300

    Add possibility to move card to the trash from card details screen

--- app/src/main/kotlin/com/popalay/cardme/utils/behaviors/FabScrollBehavior.kt
+++ app/src/main/kotlin/com/popalay/cardme/utils/behaviors/FabScrollBehavior.kt
@@ -7,8 +7,6 @@ import android.support.design.widget.FloatingActionButton
 import android.support.v4.view.ViewCompat
 import android.util.AttributeSet
 import android.view.View
-import com.popalay.cardme.utils.extensions.hideAnimated
-import com.popalay.cardme.utils.extensions.showAnimated
 
 class FabScrollBehavior(context: Context, attrs: AttributeSet) : SnackbarFabBehavior(context, attrs) {
 
@@ -19,7 +17,7 @@ class FabScrollBehavior(context: Context, attrs: AttributeSet) : SnackbarFabBeha
                                     target: View,
                                     type: Int) {
         super.onStopNestedScroll(coordinatorLayout, child, target, type)
-        handler.postDelayed({ child.showAnimated() }, 500L)
+        handler.postDelayed({ child.show() }, 500L)
     }
 
     override fun onNestedScroll(coordinatorLayout: CoordinatorLayout,
@@ -32,7 +30,7 @@ class FabScrollBehavior(context: Context, attrs: AttributeSet) : SnackbarFabBeha
                                 type: Int) {
         super.onNestedScroll(coordinatorLayout, child, target, dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed, type)
         if (dyConsumed == 0) return
-        child.hideAnimated()
+        child.hide()
     }
 
     override fun onStartNestedScroll(coordinatorLayout: CoordinatorLayout,
