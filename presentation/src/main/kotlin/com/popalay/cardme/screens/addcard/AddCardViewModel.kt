6ccce5be939810226fe0645b386e26e18842b7e7
Denis Nikiforov <popalay@ex.ua>
2017-12-26 00:15:22 +0200

    Create base classes for Redux

--- presentation/src/main/kotlin/com/popalay/cardme/screens/addcard/AddCardViewModel.kt
+++ presentation/src/main/kotlin/com/popalay/cardme/screens/addcard/AddCardViewModel.kt
@@ -1,28 +1,17 @@
 package com.popalay.cardme.screens.addcard
 
-import android.databinding.ObservableArrayList
-import android.databinding.ObservableBoolean
-import android.databinding.ObservableField
-import android.view.inputmethod.EditorInfo
-import com.jakewharton.rxrelay2.BehaviorRelay
-import com.jakewharton.rxrelay2.PublishRelay
 import com.popalay.cardme.base.BaseViewModel
 import com.popalay.cardme.base.navigation.CustomRouter
 import com.popalay.cardme.domain.interactor.CardInteractor
 import com.popalay.cardme.domain.interactor.HolderInteractor
 import com.popalay.cardme.domain.interactor.SettingsInteractor
 import com.popalay.cardme.domain.model.Card
-import com.popalay.cardme.utils.extensions.applyThrottling
-import com.popalay.cardme.utils.extensions.clean
-import com.popalay.cardme.utils.extensions.setTo
-import com.stepango.rxdatabindings.ObservableString
-import com.stepango.rxdatabindings.observe
-import com.stepango.rxdatabindings.setTo
 import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.rxkotlin.Observables
-import io.reactivex.rxkotlin.addTo
+import io.reactivex.rxkotlin.plusAssign
 import io.reactivex.rxkotlin.subscribeBy
+import io.reactivex.subjects.BehaviorSubject
+import io.reactivex.subjects.Subject
 import javax.inject.Inject
 import javax.inject.Named
 
@@ -32,43 +21,26 @@ class AddCardViewModel @Inject constructor(
         cardInteractor: CardInteractor,
         holderInteractor: HolderInteractor,
         settingsInteractor: SettingsInteractor
-) : BaseViewModel(), AddCardViewModelFacade {
-
-    val holderName = ObservableString()
-    val holderNames: ObservableArrayList<String> = ObservableArrayList()
-    val title = ObservableString()
-    val showImage = ObservableBoolean()
-    val card = ObservableField<Card>()
-
-    val editorActionListener: PublishRelay<Int> = PublishRelay.create<Int>()
-
-    private val canSaveState: BehaviorRelay<Boolean> = BehaviorRelay.create<Boolean>()
-    private val acceptClickListener: PublishRelay<Boolean> = PublishRelay.create<Boolean>()
+) : BaseViewModelNew<AddCardState, AddCardAction, AddCardSignal>() {
 
     init {
-        cardInteractor.get(cardNumber)
-                .observeOn(AndroidSchedulers.mainThread())
-                .setTo(card)
-                .doOnNext {
-                    title.set(it.title)
-                    holderName.set(it.holderName)
-                }
-                .subscribeBy(this::handleBaseError)
-                .addTo(disposables)
 
-        settingsInteractor.listenShowCardsBackground()
+        disposables += cardInteractor.get(cardNumber)
                 .observeOn(AndroidSchedulers.mainThread())
-                .setTo(showImage)
+                .doOnNext { dispatch(AddCardAction.CardResultSuccess(it)) }
                 .subscribeBy(this::handleBaseError)
-                .addTo(disposables)
 
-        holderInteractor.getNames()
+        disposables += settingsInteractor.listenShowCardsBackground()
                 .observeOn(AndroidSchedulers.mainThread())
-                .setTo(holderNames)
+                .doOnNext { dispatch(AddCardAction.CardBackgroundResultSuccess(it)) }
                 .subscribeBy(this::handleBaseError)
-                .addTo(disposables)
 
-        Observables.combineLatest(
+        disposables += holderInteractor.getNames()
+                .observeOn(AndroidSchedulers.mainThread())
+                .doOnNext { dispatch(AddCardAction.HolderNamesResultSuccess(it)) }
+                .subscribeBy(this::handleBaseError)
+
+        /*Observables.combineLatest(
                 holderName.observe().doOnNext { card.set(card.get()?.copy(holderName = it.clean())) },
                 title.observe().doOnNext { card.set(card.get()?.copy(title = it.clean())) })
                 .switchMapSingle { cardInteractor.hasAllData(card.get(), it.first) }
@@ -92,16 +64,80 @@ class AddCardViewModel @Inject constructor(
                 .filter { it == EditorInfo.IME_ACTION_DONE }
                 .doOnNext { acceptClickListener.accept(true) }
                 .subscribeBy(this::handleBaseError)
-                .addTo(disposables)
+                .addTo(disposables)*/
     }
 
-    override fun onAcceptClick() = acceptClickListener.accept(true)
+    override fun reduce(oldState: AddCardState?, action: AddCardAction): AddCardState {
+        val state = oldState ?: AddCardState()
+        return when (action) {
+            is AddCardAction.CardResultSuccess -> state.copy(card = action.card)
+            is AddCardAction.HolderNamesResultSuccess -> state.copy(holderNames = action.holderNames)
+            is AddCardAction.CardBackgroundResultSuccess -> state.copy(showBackground = action.showCardBackground)
+            is AddCardAction.CardValidationSuccess -> state.copy(canSave = action.valid)
+        }
+    }
 
-    override fun onCanSaveStateChanged(): Observable<Boolean> = canSaveState
-
+    override fun handle(signal: AddCardSignal) {
+        when (signal) {
+            is AddCardSignal.NameChangedSignal -> TODO()
+            is AddCardSignal.TitleChangedSignal -> TODO()
+            is AddCardSignal.CardValidationSuccess -> TODO()
+            is AddCardSignal.AcceptSignal -> TODO()
+        }
+    }
 }
 
-interface AddCardViewModelFacade {
-    fun onAcceptClick()
-    fun onCanSaveStateChanged(): Observable<Boolean>
+sealed class AddCardAction : Action {
+    data class CardResultSuccess(val card: Card) : AddCardAction()
+    data class HolderNamesResultSuccess(val holderNames: List<String>) : AddCardAction()
+    data class CardBackgroundResultSuccess(val showCardBackground: Boolean) : AddCardAction()
+    data class CardValidationSuccess(val valid: Boolean) : AddCardAction()
+}
+
+sealed class AddCardSignal : Signal {
+    data class NameChangedSignal(val name: String) : AddCardSignal()
+    data class TitleChangedSignal(val title: String) : AddCardSignal()
+    data class CardValidationSuccess(val valid: Boolean) : AddCardSignal()
+    object AcceptSignal : AddCardSignal()
+}
+
+data class AddCardState(
+        val card: Card = Card(),
+        val showBackground: Boolean = false,
+        val holderNames: List<String> = listOf(),
+        val canSave: Boolean = false
+) : State
+
+interface Action
+
+interface State
+
+interface Signal
+
+interface Dispatcher<in A : Action> {
+    fun dispatch(action: A)
+}
+
+interface Handler<in SI : Signal> {
+    fun handle(signal: SI)
+}
+
+interface Reducer<S : State, in A : Action> {
+    fun reduce(oldState: S?, action: A): S
+}
+
+interface StateProvider<S : State> {
+    fun newState(): Observable<S>
+}
+
+abstract class BaseViewModelNew<S : State, in A : Action, in SI : Signal>
+    : BaseViewModel(), Dispatcher<A>, Reducer<S, A>, StateProvider<S>, Handler<SI> {
+
+    private val stateSubject: Subject<S> = BehaviorSubject.create<S>()
+
+    override fun dispatch(action: A) {
+        reduce(stateSubject.blockingLast(null), action).also(stateSubject::onNext)
+    }
+
+    override fun newState(): Observable<S> = stateSubject
 }
