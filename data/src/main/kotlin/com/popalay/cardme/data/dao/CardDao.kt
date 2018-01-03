78dc24278dad25728bcb82dff4c0d074d0a834b1
Denis Nikiforov <popalay@ex.ua>
2018-01-04 00:15:02 +0200

    - Refactoring of Card data model

--- data/src/main/kotlin/com/popalay/cardme/data/dao/CardDao.kt
+++ data/src/main/kotlin/com/popalay/cardme/data/dao/CardDao.kt
@@ -5,37 +5,37 @@ import android.arch.persistence.room.Delete
 import android.arch.persistence.room.OnConflictStrategy.REPLACE
 import android.arch.persistence.room.Query
 import android.arch.persistence.room.Update
+import com.popalay.cardme.data.model.Card
 import io.reactivex.Flowable
 import io.reactivex.Single
-import com.popalay.cardme.data.model.Card as DataCard
 
 @Dao
-interface CardDao : BaseDao<DataCard> {
+interface CardDao : BaseDao<Card> {
 
     @Query("SELECT * FROM cards " +
             "WHERE isPending = 0 " +
             "ORDER BY position, holderName")
-    fun getAll(): Flowable<List<DataCard>>
+    fun getAll(): Flowable<List<Card>>
 
     @Query("SELECT * FROM cards " +
             "WHERE number = :number " +
             "LIMIT 1")
-    fun get(number: String): Flowable<DataCard>
+    fun get(number: String): Flowable<Card>
 
     @Query("SELECT * FROM cards " +
             "WHERE isTrash = 0 AND isPending = 0 " +
             "ORDER BY position, holderName")
-    fun getAllNotTrashed(): Flowable<List<DataCard>>
+    fun getAllNotTrashed(): Flowable<List<Card>>
 
     @Query("SELECT * FROM cards " +
             "WHERE isTrash = 0 AND isPending = 0 AND holderName = :holderName " +
             "ORDER BY position, holderName")
-    fun getNotTrashedByHolder(holderName: String): Flowable<List<DataCard>>
+    fun getNotTrashedByHolder(holderName: String): Flowable<List<Card>>
 
     @Query("SELECT * FROM cards " +
             "WHERE isTrash = 1 AND isPending = 0 " +
             "ORDER BY position, holderName")
-    fun getAllTrashed(): Flowable<List<DataCard>>
+    fun getAllTrashed(): Flowable<List<Card>>
 
     @Query("SELECT COUNT (*) FROM cards " +
             "WHERE isTrash = 0 AND isPending = 0 AND number = :number")
@@ -46,9 +46,9 @@ interface CardDao : BaseDao<DataCard> {
     fun getCountByHolder(holderName: String): Flowable<Int>
 
     @Update(onConflict = REPLACE)
-    fun updateAll(cards: List<DataCard>)
+    fun updateAll(cards: List<Card>)
 
     @Delete
-    fun deleteAll(cards: List<DataCard>)
+    fun deleteAll(cards: List<Card>)
 
 }
